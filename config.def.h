69cb95b0c1167fa791723800dff630496f8a88c2
Volka0 <134385522+Volka0@users.noreply.github.com>
2023-05-26 12:47:45 -0400

    New colorscheme Transfer and Active Indicators patch applied and merged + Fixed errors

--- config.def.h
+++ config.def.h
@@ -9,15 +9,16 @@ static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static unsigned int baralpha        = 0xd0;
 static unsigned int borderalpha     = OPAQUE;
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char norm_fg[]         = "#0abdc6";
+static const char norm_bg[]         = "#091833";
+static const char norm_border[]     = "#091833";
+static const char sel_fg[]          = "#ea00d9";
+static const char sel_bg[]          = "#321959";
+static const char sel_border[]      = "#ea00d9";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    /*               fg       bg       border   */
+    [SchemeNorm] = { norm_fg, norm_bg, norm_border },
+    [SchemeSel]  = { sel_fg,  sel_bg,  sel_border  },
 };
 
 /* tagging */
@@ -59,7 +60,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+/* Adjust dmenu command accordingly */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn",dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
