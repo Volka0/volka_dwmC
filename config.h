e39058b154747e4517be4e37ad209401f35cd6ac
Volka0 <134385522+Volka0@users.noreply.github.com>
2023-05-26 12:33:46 -0400

    New colorscheme Transfer and Active Indicators patch applied and merged

--- config.h
+++ config.h
@@ -7,6 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 static const char *fonts[]          = { "HackNerdFontMono-Regular:size=14" };
 static const char dmenufont[]       = "HackNerdFontMono-Regular:size=14";
 static unsigned int baralpha        = 0xd0;
@@ -21,21 +22,33 @@ static const char sel_border[]      = "#ea00d9";
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 =======
+=======
+>>>>>>> New colorscheme Transfer and Active Indicators patch applied and merged
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static unsigned int baralpha        = 0xd0;
 static unsigned int borderalpha     = OPAQUE;
+<<<<<<< HEAD
 >>>>>>> Compiles succesfully
+=======
+>>>>>>> New colorscheme Transfer and Active Indicators patch applied and merged
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+<<<<<<< HEAD
 >>>>>>> Transfer Patch
 static const char *colors[][3]      = {
     /*               fg       bg       border   */
     [SchemeNorm] = { norm_fg, norm_bg, norm_border },
     [SchemeSel]  = { sel_fg,  sel_bg,  sel_border  },
+=======
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+>>>>>>> New colorscheme Transfer and Active Indicators patch applied and merged
 };
 
 /* tagging */
@@ -77,8 +90,12 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+<<<<<<< HEAD
 /* Adjust dmenu command accordingly */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn",dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
+=======
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+>>>>>>> New colorscheme Transfer and Active Indicators patch applied and merged
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
